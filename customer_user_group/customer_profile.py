80ca96bfa550d8f947e53786ab685d5bb4db4030
erik-whiting <erik@erikwhiting.com>
2020-08-23 22:00:09 -0500

    Reduce wait times for dev purposes

--- customer_user_group/customer_profile.py
+++ customer_user_group/customer_profile.py
@@ -1,17 +1,19 @@
 import random, time
 
 class CustomerProfile:
+  # For testing purposes, I am making the wait times much shorter than
+  # they probably would be in a real life situation.
   def __init__(self):
     self.description = 'Typical customer usage'
   
   def linger(self):
-    time.sleep(random.randrange(2, 10))
+    time.sleep(random.randrange(1, 5))
 
   def get_distracted(self):
-    time.sleep(random.randrange(30, 60))
+    time.sleep(random.randrange(3, 7))
   
   def go_to_bathroom(self):
-    time.sleep(random.randrange(60, 120))
+    time.sleep(random.randrange(5, 9))
   
   def go_to_lunch(self):
-    time.sleep(random.randrange(120, 240))
+    time.sleep(random.randrange(7, 11))
--- employee_user_group/employee_profile.py
+++ employee_user_group/employee_profile.py
@@ -1,12 +1,14 @@
 import random, time
 
 class EmployeeProfile:
+  # For testing purposes, I am making the wait times much shorter than
+  # they probably would be in a real life situation.
   def __init__(self):
     self.description = 'Typical employee usage'
   
   def linger(self):
-    time.sleep(random.randrange(2, 10))
+    time.sleep(random.randrange(1, 5))
   
   def double_check(self):
-    time.sleep(random.randrange(10, 30))
+    time.sleep(random.randrange(2, 8))
     
